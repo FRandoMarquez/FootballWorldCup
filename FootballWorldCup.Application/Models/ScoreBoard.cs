c8facf7c322c9c5e43979a2dc3f6c46c4bc0cdb2
francisco.j.rando <francisco.j.rando@hotmail.com>
2022-09-08 20:40:06 +0200

    feat: Code ToString method for ScoreBoard class

--- FootballWorldCup.Application/Models/ScoreBoard.cs
+++ FootballWorldCup.Application/Models/ScoreBoard.cs
@@ -9,5 +9,16 @@ namespace FootballWorldCup.Application.Models
     public class ScoreBoard
     {
         public List<Game> Games { get; set; }
+
+        public override string ToString()
+        {
+            string scoreBoardToString = string.Empty;
+            for (int i = 0; i < Games.Count; i++)
+            {
+                scoreBoardToString += string.Format("{0}. {1} {2} - {3} {4}", i, Games[i].HomeTeam, Games[i].HomeTeamScore, Games[i].AwayTeam, Games[i].AwayTeamScore)
+                    + Environment.NewLine;
+            }
+            return scoreBoardToString;
+        }
     }
 }
